b9e7828012aebe8fab73f2bf4cfd893e4040654f
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-03-12 16:45:39 +0100

    trait and extends

--- index.php
+++ index.php
@@ -15,29 +15,88 @@
         }
     };
 
+    trait DiscountApplicable {
+        public function applyDiscount($discountPercentage)
+        {
+            $discountedPrice = $this->price * (1 - $discountPercentage / 100);
+            return number_format($discountedPrice, 2, '.', '') . ' €';
+        }
+    };
+
+
+    class Accessory extends Product {
+        use DiscountApplicable;
+        public $material;
+        public $size;
+
+        function __construct($img, $animal, $title, $price, $category, $material, $size)
+        {
+            parent::__construct($img, $animal, $title, $price, $category);
+            $this->material = $material;
+            $this->size = $size;
+        }
+    };
+
+    class Food extends Product {
+        use DiscountApplicable;
+        public $expiryDate;
+        public $calories;
+
+        function __construct($img, $animal, $title, $price, $category, $expiryDate, $calories)
+        {
+            parent::__construct($img, $animal, $title, $price, $category);
+            $this->expiryDate = $expiryDate;
+            $this->calories = $calories;
+        }
+    };
+
+    class Toy extends Product {
+        use DiscountApplicable;
+        public $age;
+        public $batteries;
+
+        function __construct($img, $animal, $title, $price, $category, $age, $batteries)
+        {
+            parent::__construct($img, $animal, $title, $price, $category);
+            $this->age = $age;
+            $this->batteries = $batteries;
+        }
+
+        function hasBatteries() {
+            return $this->batteries ? 'Batteries included' : 'Batteries not included';
+        }
+    };
+
+
     $productsArray = [ 
-        $harness = new Product(
+        $harness = new Accessory(
             'https://www.jurassicbark-online.co.uk/cdn/shop/products/hurttahedge_90c1c259-cf06-481c-b3de-0c110e93ba27_570x617_crop_top.jpg?v=1680095344',
             'dog',
             'Harness',
             45.99,
-            'Accessories'
+            'Accessories',
+            'Polyester',
+            'M'
         ),
-        $wet85g = new Product(
+        $wet85g = new Food(
             'https://www.jurassicbark-online.co.uk/cdn/shop/products/NSEMEAACANAPremiumPateWetCatFoodChickenFront85g_232fbcce-16f6-4590-a4d7-a7af597b5743_570x617_crop_top.png?v=1653382264',
             'cat',
             'Wet Food 85g',
             1.49,
-            'Food'
+            'Food',
+            '10/2026',
+            102
         ),
-        $ballLauncher = new Product(
+        $ballLauncher = new Toy(
             'https://www.jurassicbark-online.co.uk/cdn/shop/products/Medium18_570x617_crop_top.png?v=1647263341',
             'dog',
             'Ball Launcher',
             12.99,
-            'Toys'
+            'Toys',
+            'Adult',
+            false
         )
-     ]
+    ];
     
     
 ?>
@@ -82,11 +141,26 @@
                     <div class="card-body">
                         <h5 class="card-title"><?= $product->title ?></h5>
                         <h6 class="card-text">
-                            <?= $product->price ?> €
+                        <?= $product->price ?> € |
+                            <?php 
+                                try {
+                                    echo 'Discounted price: ' . $product->applyDiscount(10);
+                                } catch (Exception $e) {
+                                    echo "Error formatting price: " . $e->getMessage();
+                                }
+                            ?> 
                         </h6>
                     </div>
                     <div class="card-footer text-body-secondary">
                         <?= $product->category ?>
+                    </div>
+                    <div class="card-body">
+                        <h6 class="mt-5">Additional informations:</h6>
+                        <span class="card-title"><?= $product->material ?? $product->expiryDate ?? $product->age ?></span>
+                        <br>
+                        <span class="card-text">
+                            <?= $product->taglia ?? $product->calories ?? ($product instanceof Toy ? $product->hasBatteries() : '') ?>
+                </span>
                     </div>
                 </div>
                 <?php endforeach ?>
